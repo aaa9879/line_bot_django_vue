0f055d223b1f00319148499d349eebc9487533a2
Wang Sheng He <134773871+aaa9879@users.noreply.github.com>
2024-06-02 17:08:04 +0800

    6/2

--- project/module/langchain_tool.py
+++ project/module/langchain_tool.py
@@ -4,8 +4,9 @@ class category_classification(BaseTool):
     name = "account_classification_tool"
     description = (
         """
-        這是帳目分類器，請從使用者輸入和類別進行下列判斷
-        "類別"：從使用者給予的類別當中的值進行挑選，一定要從中選出一個不能自己產生，只需要輸出類別就好
+        為帳目分類，請從給予的類別判斷使用者輸入屬於哪個類別，
+        類別請從使用者給予的類別當中的值進行挑選，一定要從中選出一個不能自己產生，只需要輸出類別就可以
+        ex:抓出"餐費"輸出"餐費"，不要隨意產生一段文字
         """
     )
 
@@ -13,7 +14,7 @@ class category_classification(BaseTool):
             類別: str = None,
              ):
 
-        return(f"category：{類別}")
+        return(類別)
 
 def get_category_classification_tool(llm):
     tools = [category_classification()]
@@ -24,28 +25,3 @@ def get_category_classification_tool(llm):
         agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         handle_parsing_errors=True,
         verbose=True)
-
-
-def create_item_name_tool(llm):
-    tools = [create_item_name()]
-
-    return initialize_agent(
-        tools,
-        llm,
-        agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-        handle_parsing_errors=True,
-        verbose=True)
-
-class create_item_name(BaseTool):
-    name = "create_item_name_tool"
-    description = (
-        """
-        這是一筆花費的花費項目名稱生成器，請根據使用者輸入，生成合適的花費名稱，例如漢堡、 衣服、薪水等等不用動詞也就是吃漢堡就漢堡就可以
-        """
-    )
-
-    def _run(self,
-             項目: str = None
-             ):
-
-        return (f"item:{項目}")
