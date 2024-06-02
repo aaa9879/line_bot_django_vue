6514fff704e79e842c371316d81575884282b04b
Wang Sheng He <134773871+aaa9879@users.noreply.github.com>
2024-06-02 17:09:47 +0800

    6/2

--- project/line_bot_app/api_urls.py
+++ project/line_bot_app/api_urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('get_personal_account/', views.get_user_account, name='get_user_account_api'),
-    
+    path('get_user_account_info/',views.get_user_account_info,name = 'get_user_account_info_api'),
+    path('get_keep_temporary/',views.get_keep_temporary,name = 'get_keep_temporary_api'),
+    path('get_keep_sure/',views.get_keep_sure,name = 'get_keep_sure_api'),
 ]
