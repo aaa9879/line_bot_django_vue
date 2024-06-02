2f6feb9f262c0ca43d2e366637be4d37cb3da262
Wang Sheng He <134773871+aaa9879@users.noreply.github.com>
2024-06-02 17:12:46 +0800

    6/2

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 Vue.prototype.$apiUrl = 'https://6ccf-58-114-6-244.ngrok-free.app';
 Vue.prototype.$axios = axios;
 Vue.prototype.$userId = null;
-Vue.prototype.$userName = null;//6/2
+Vue.prototype.$userName = null;// 6/2
 Vue.use(Calendar);
 Vue.config.productionTip = false
 /* eslint-disable no-new */
