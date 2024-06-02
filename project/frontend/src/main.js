612ac4c436475ecd985b16d9635054b8b3fe413e
Wang Sheng He <134773871+aaa9879@users.noreply.github.com>
2024-06-02 17:12:32 +0800

    Update main.js

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -3,9 +3,10 @@ import App from './App'
 import router from './router'
 import Calendar from 'vue-mobile-calendar'
 import axios from 'axios';
-Vue.prototype.$apiUrl = 'https://b2bd-2401-e180-8841-3a0e-c93c-e11a-fe96-1ff9.ngrok-free.app';
+Vue.prototype.$apiUrl = 'https://6ccf-58-114-6-244.ngrok-free.app';
 Vue.prototype.$axios = axios;
 Vue.prototype.$userId = null;
+Vue.prototype.$userName = null;//6/2
 Vue.use(Calendar);
 Vue.config.productionTip = false
 /* eslint-disable no-new */
