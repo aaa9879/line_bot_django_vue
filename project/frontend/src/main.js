38861259b82065566770a4c0678208ba862d2907
Wang Sheng He <134773871+aaa9879@users.noreply.github.com>
2024-06-05 17:00:19 +0800

    6/5

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App'
 import router from './router'
 import Calendar from 'vue-mobile-calendar'
 import axios from 'axios';
-Vue.prototype.$apiUrl = 'https://6ccf-58-114-6-244.ngrok-free.app';
+Vue.prototype.$apiUrl = 'https://fd27-2402-7500-489-643e-a45e-9ea5-3762-c424.ngrok-free.app';
 Vue.prototype.$axios = axios;
 Vue.prototype.$userId = null;
 Vue.prototype.$userName = null;// 6/2
